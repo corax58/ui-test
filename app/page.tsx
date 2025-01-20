acc27dd677f072f342cdaadb480a0c0a9718b3e1
Abubeker Abduljelil <abubekera87@gmail.com>
2025-01-20 21:03:20 +0300

    Disable on smaller devices

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,18 @@
+import { TbCircleX } from "react-icons/tb";
 import MainSection from "./components/MainSection/";
 import SideBar from "./components/SideBar/";
 
 export default function Home() {
   return (
     <div className="w-screen h-screen flex ">
-      <SideBar />
-      <MainSection />
+      <div className=" flex gap-2 p-2 font-bold bg-black text-white lg:hidden h-full w-full items-center justify-center">
+        <TbCircleX size={60} />
+        This site only works on desktop, please switch to desktop mode.
+      </div>
+      <div className=" hidden lg:flex h-screen">
+        <SideBar />
+        <MainSection />
+      </div>
     </div>
   );
 }
